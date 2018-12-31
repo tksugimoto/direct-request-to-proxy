e7eec973c5e4a874a0cfa3a9571f13187b556799
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-01-01 00:13:22 +0900

    feat: HTTPリクエストをHTTPプロキシサーバーに転送するHTTPサーバーを作成

---
+++ http-server/index.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const net = require('net');
+const {
+    parse: parseUrl,
+} = require('url');
+
+assert(process.env.http_proxy, 'http_proxy env ("http://hostname:port") required.');
+
+const {
+    hostname: httpProxyHost,
+    port: httpProxyPort,
+} = parseUrl(process.env.http_proxy);
+
+
+const CRLF = '\r\n';
+const HTTP_PORT = 80;
+
+const currentTime = () => {
+    const date = new Date();
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    const ms = date.getMilliseconds();
+    return `${h}:${m}:${s}.${ms}`;
+};
+
+const log = text => {
+    console.info(`[${currentTime()}] ------------- ${text} -------------`);
+};
+
+const httpServer = net.createServer();
+
+httpServer.on('connection', (clientSocket) => {
+    clientSocket.once('data', dataBuffer => {
+        const httpRequestArray = dataBuffer.toString().split(CRLF);
+        const hostHeader = httpRequestArray.find(header => header.startsWith('Host:'));
+        if (!hostHeader) return;
+
+        const proxyServerSocket = net.createConnection(httpProxyPort, httpProxyHost);
+        proxyServerSocket.once('connect', () => {
+            const hostname = hostHeader.slice('Host:'.length).trim().replace(/:.*/, '');
+            log(`forward request to ${hostname}`);
+            httpRequestArray[0] = httpRequestArray[0].replace(' ', ` http://${hostname}`);
+            proxyServerSocket.write(httpRequestArray.join(CRLF));
+            clientSocket.pipe(proxyServerSocket);
+            proxyServerSocket.pipe(clientSocket);
+        });
+        proxyServerSocket.on('error', err => {
+            clientSocket.end(err.message);
+        });
+        clientSocket.on('error', () => {
+            proxyServerSocket.destroy();
+        });
+    });
+});
+
+httpServer.listen(HTTP_PORT, '0.0.0.0');
